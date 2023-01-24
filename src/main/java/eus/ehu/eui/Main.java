f9bdbdf2d024ae0fdd7127d4dcbd2f19d36a3e37
ijurko8 <iijurco004@ikasle.ehu.eus>
2023-01-24 17:13:59 +0100

    Segunda versión

--- src/main/java/eus/ehu/eui/Main.java
+++ src/main/java/eus/ehu/eui/Main.java
@@ -3,6 +3,6 @@ package eus.ehu.eui;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Otra version!");
     }
 }
