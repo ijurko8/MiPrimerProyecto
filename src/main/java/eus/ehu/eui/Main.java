72a83182cfe3159dce57a4e251e654cc30c9f74c
ijurko8 <iijurco004@ikasle.ehu.eus>
2023-01-24 17:16:53 +0100

    Versión alternativa

--- src/main/java/eus/ehu/eui/Main.java
+++ src/main/java/eus/ehu/eui/Main.java
@@ -3,6 +3,6 @@ package eus.ehu.eui;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Otra version!");
+        System.out.println("Este es un nuevo mensaje!");
     }
 }
